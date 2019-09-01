e47d5c1a45c3e4a8b00a2a68d34cc9b5495a1215
Matthieu <alivecreation.studio@gmail.com>
2019-09-01 18:31:15 +0400

    Migration readd

---
+++ HeroBot.Plugins.RP/Entities/RPUser.cs
@@ -0,0 +1,15 @@
+﻿namespace HeroBot.Plugins.RP.Entities
+{
+    public class RPUser
+    {
+        public string Id { get; set; }
+        public string CityId { get; set; }
+        public string Description { get; set; }
+        public string Website { get; set; }
+        public string Emoji { get; set; }
+        public string Personality { get; set; }
+        public long Likes { get; set; }
+        public Jobs Job { get; set; }
+        public long Money { get; set; }
+    }
+}
